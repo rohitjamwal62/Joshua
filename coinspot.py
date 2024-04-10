6adf7d9e36cceee0f9b2f781dab2461d71b27f44
rohitjamwal62 <jamwalrohit06@gmail.com>
2024-04-10 16:10:16 +0530

    new

---
+++ coinspot.py
@@ -0,0 +1,36 @@
+
+
+# Replace these with your actual CoinSpot API keys
+API_KEY = '65ef7ebac655caceefdb71c419a91202'
+API_SECRET = 'T1WG4REYWQA0XK8V7QCP2XGF8NK8BJQ1LR7RGVGHMRBLP22AXQ36K3T1LKGCKD3LB9RUUUUQ2KN6VAM9'
+
+import requests,hashlib,hmac,time,json
+
+
+BALANCE_URL = 'https://www.coinspot.com.au/api/my/balances'
+# BALANCE_URL = "https://www.coinspot.com.au/api/v2/status"
+nonce = int(time.time())
+params = {
+    'nonce': nonce
+}
+json_params = json.dumps(params, separators=(',' , ':') )
+signature = hmac.new(API_SECRET.encode(), json_params.encode(), hashlib.sha512).hexdigest()
+headers = {
+    'Content-Type': 'application/json',
+    'key': API_KEY,
+    'sign': signature,
+
+}
+
+response = requests.post(BALANCE_URL, headers=headers, json=params).json()
+print(response,"====")
+
+# # Check if request was successful
+# if response.status_code == 200:
+#     print(response.text,"===")
+#     balances = response.json()['balance']
+#     print("Your balances:")
+#     for currency, amount in balances.items():
+#         print(f"{currency}: {amount}")
+# else:
+#     print("Failed to retrieve balance:", response.text)
--- match_string.py
+++ match_string.py
@@ -1,13 +1,7 @@
 import re
 def StopLoss(String):
-    store_list = list()
-    stop_loss_pattern = r"STOP LOSS: (\d+\.\d+)"
-    stop_loss_match = re.search(stop_loss_pattern, String)
-    if stop_loss_match:
-        stop_loss_value = stop_loss_match.group(1)
-        print("Stop Loss:", stop_loss_value)
-        store_list.append(stop_loss_match)
-    return stop_loss_value
+    Text = str(String).split()[-1]
+    return Text
 
         
     
@@ -17,6 +11,16 @@ Pair: $KSM/USDTDirection: Long
 Exchanges: ByBit USDTLeverage: 3x
 ENTRY: 40 -41.5 -  42.85
 TARGETS: 43.5 - 44 - 45 - 46 - 48 - 50 - 52 - 55 - 58 - 61
-STOP LOSS: 38.73
+STOP LOSS: 6
 """
 data = StopLoss(String)
+entry = float(re.findall(r'ENTRY:\s*([\d.-]+)', String)[0])
+print(entry,"===")
+stop_loss = float(re.findall(r'STOP LOSS:\s*([\d.-]+)', String)[0])
+
+if entry - stop_loss >= 5 and entry - stop_loss <= 10:
+    print("yess")
+    
+# - I can adjust the parameters myself (i.e. stop loss percentage from 3% to 10%, or the profit sell off from 10% to 20% of purchase price)
+
+    
